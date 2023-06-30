4e965a1644d6b5432e5085ed28fcf890b949a183
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-30 14:44:38 +0300

    Added Order model

--- cinema/models.py
+++ cinema/models.py
@@ -65,3 +65,16 @@ class MovieSession(models.Model):
 
     def __str__(self):
         return self.movie.title + " " + str(self.show_time)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
