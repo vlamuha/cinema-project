f2ab8cf9fa1a53db246395238db4d7a6babd51bd
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-30 14:42:02 +0300

    Added CinemaHall model

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
