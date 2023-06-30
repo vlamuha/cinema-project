911df2410bca7391e2022c44455d239a53680b7d
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-06-30 14:42:27 +0300

    Added Genre model

--- cinema/models.py
+++ cinema/models.py
@@ -13,3 +13,9 @@ class CinemaHall(models.Model):
     def __str__(self):
         return self.name
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
